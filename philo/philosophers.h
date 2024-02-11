f348a54b44a45f6e366537a29a223fe8a1a1c803
AllanDantas21 <allandantas21.ad@gmail.com>
2024-02-11 17:06:02 -0300

    add libs

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,10 +6,14 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 17:02:41 by aldantas          #+#    #+#             */
-/*   Updated: 2024/02/11 17:03:15 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/02/11 17:05:49 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
 
 #endif
