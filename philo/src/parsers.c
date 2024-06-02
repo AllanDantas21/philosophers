1dfb1887fc081fbe878638fe6a6c03e65906ffdd
Allandantas21 <Allandantas21.ad@gmail.com>
2024-06-02 16:24:57 -0300

    fix segfault

--- philo/src/parsers.c
+++ philo/src/parsers.c
@@ -53,7 +53,7 @@ int	parse_data(t_data *data, char **av)
 		return (-1);
 	if (pthread_mutex_init(&data->print_mtx, NULL))
 		return (-1);
-	data->array_forks = malloc(sizeof(pthread_mutex_t) * data->philo_nbr);
+	data->array_forks = malloc(sizeof(t_fork) * data->philo_nbr);
 	data->array_philos = malloc(sizeof(t_philo *) * data->philo_nbr);
 	if (!data->array_forks || !data->array_forks)
 		return (-1);
